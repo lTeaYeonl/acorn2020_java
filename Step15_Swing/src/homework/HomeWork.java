a1b38ce4f1526239c85f3725b09107824f9c26e9
kimji <kimji@DESKTOP-FK0Q0L2>
2020-06-14 22:52:07 +0900

    과제 예외처리 업데이트 (정수형만 가능, 실수형 불가능)

--- Step15_Swing/src/homework/HomeWork.java
+++ Step15_Swing/src/homework/HomeWork.java
@@ -7,6 +7,7 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 
 public class HomeWork extends JFrame implements ActionListener{
@@ -81,36 +82,57 @@ public class HomeWork extends JFrame implements ActionListener{
 	// 버튼을 눌렀을때 실행되어질 메소드
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		String command=e.getActionCommand();
-		if(command.equals(COMMAND_ADDITION)) { // 덧셈
-			convert();
-			sum=num1+num2;
-			convert2();
-			reset();			
-		} else if(command.equals(COMMAND_SUBTRACTION)) { // 뺼셈
-			convert();
-			sum=num1-num2;
-			convert2();
-			reset();	
-		} else if(command.equals(COMMAND_MULTIPLICATION)) { // 곱셈
-			convert();
-			sum=num1*num2;
-			convert2();
-			reset();	
-		} else if(command.equals(COMMAND_DIVISION)) { // 나눗셈
-			convert();
-			sum=num1/num2;
-			convert2();
-			reset();	
+		try {
+			String command=e.getActionCommand();
+			if(command.equals(COMMAND_ADDITION)) { // 덧셈
+				convert();
+				sum=num1+num2;
+				convert2();
+				reset();			
+			} else if(command.equals(COMMAND_SUBTRACTION)) { // 뺼셈
+				convert();
+				sum=num1-num2;
+				convert2();
+				reset();	
+			} else if(command.equals(COMMAND_MULTIPLICATION)) { // 곱셈
+				convert();
+				sum=num1*num2;
+				convert2();
+				reset();	
+			} else if(command.equals(COMMAND_DIVISION)) { // 나눗셈
+				convert();
+				sum=num1/num2;
+				convert2();
+				reset();	
+			}
+		} catch(NumberFormatException nfe) {
+			JOptionPane.showMessageDialog(this, "숫자만 입력해 주세요!");
+			// 예외 정보 콘솔창에 출력
+			nfe.printStackTrace();
+			reset();
+		} catch (ArithmeticException ae) {
+			JOptionPane.showMessageDialog(this, "어떤 수를 0으로 나눌 수는 없어요!");
+			ae.printStackTrace();
+			reset();
+		} catch (Exception e1 ) {
+			JOptionPane.showMessageDialog(this, "Exception이 발생했습니다!");
+			reset();
 		}
 	}
 	
 	// JTextfield에 입력된 값을 int 타입으로 변환해주는 메소드
 	public void convert() {
-		String text1=inputNum1.getText();
-		num1=Integer.parseInt(text1);
-		String text2=inputNum2.getText();
-		num2=Integer.parseInt(text2);
+		if (inputNum1.getText().isEmpty() && inputNum1.getText().isEmpty()) {
+			JOptionPane.showMessageDialog(this, "값을 입력해 주세요!");
+			reset();
+			answer.setText("0");
+			return;
+		} else {
+			String text1=inputNum1.getText();
+			num1=Integer.parseInt(text1);
+			String text2=inputNum2.getText();
+			num2=Integer.parseInt(text2);
+			}
 	}
 	// 컨버팅 2
 	public void convert2() {
