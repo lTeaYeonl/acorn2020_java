f096f6f162f03a9f84006f42f5b09d89a2de2a12
kimjihun <clairiamport04@gmail.com>
2020-06-22 18:17:28 +0900

    MemberDao 회원목록 메소드 수정

--- Step19_JDBC/src/test/dao/MemberDao.java
+++ Step19_JDBC/src/test/dao/MemberDao.java
@@ -6,109 +6,122 @@ import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.swing.text.html.HTMLDocument.HTMLReader.PreAction;
-
 import test.dto.MemberDto;
 import test.util.DBConnect;
 
 /*
- * 		DAO (Data Access Object) 의 약자
- * 
- * 		- 만드는 방법
- * 
- * 		1. 외부에서 객체 생성하지 못하도록 생성자의 접근 지정자를 private 로 지정
- * 		2. 자신의 참조값을 저장할 수 있는 필드를 private 로 선언
- * 		3. 자신의 참조값을 오직 하나만 생성해서 리턴해주는 static 메소드 만들기
- * 		4. 나머지 기능(select, insert, update, delete)들은 non static 으로 만들기
+ *   DAO (Data Access Object)  의 약자
+ *   
+ *   - 만드는 방법
+ *   
+ *   1. 외부에서 객체 생성하지 못하도록 생성자의 접근 지정자를 private 로 지정
+ *   2. 자신의 참조값을 저장할수 있는 필드를 private static 로 선언
+ *   3. 자신의 참조값을 오직 하나만 생성해서 리턴해주는 static 메소드 만들기 
+ *   4. 나머지 기능(select,insert,update,delete)들은 non static 으로 만들기
+ *   
  */
 public class MemberDao {
-	// 자신의 참조값을 저장할 private 필드
+	//자신의 참조값을 저장할 private 필드
 	private static MemberDao dao;
 	
-	// 외부에서 객체 생성하지 못하도록
+	//외부에서 객체 생성하지 못하도록 한다. 
 	private MemberDao() {}
 	
-	// 참조값을 리턴해주는 메소드
+	//참조값을 리턴해주는 메소드
 	public static MemberDao getInstance() {
-		if(dao==null) { // 최초 호출되면 null 이므로
-			dao=new MemberDao(); // 객체를 생성해서 static 필드에 담는다.
+		if(dao==null) {//최초 호출되면 null 이므로 
+			dao=new MemberDao();//객체를 생성해서 static 필드에 담는다. 
 		}
 		return dao;
 	}
-	// 회원 한명의 정보를 리턴해주는 메소드
+	//회원 한명의 정보를 리턴해주는 메소드
 	public MemberDto getData(int num) {
+		//회원 한명의 정보를 담을 MemberDto 
 		MemberDto dto=null;
+		
 		Connection conn=null;
 		PreparedStatement pstmt=null;
 		ResultSet rs=null;
 		try {
+			//DBConnect 객체를 이용해서 Connection 객체의 참조값을 얻어온다.
 			conn=new DBConnect().getConn();
-			String sql="SELECT *"
+			//실행할 sql 문
+			String sql="SELECT name,addr"
 					+ " FROM member"
 					+ " WHERE num=?";
 			pstmt=conn.prepareStatement(sql);
 			pstmt.setInt(1, num);
-			pstmt.executeUpdate();
-			System.out.println("member 테이블의 "+num+" 번째 회원정보를 출력합니다.");
-		} catch (Exception e) {
+			//query 문 수행하고 결과 얻어오기
+			rs=pstmt.executeQuery();
+			//반복문 돌면서 select 된 회원정보  읽어오기
+			while(rs.next()) {
+				//MemberDto 객체 생성해서 
+				dto=new MemberDto();
+				//회원 한명의 정보를 담는다. 
+				dto.setNum(num);
+				dto.setName(rs.getString("name"));
+				dto.setAddr(rs.getString("addr"));
+			}
+		}catch(Exception e) {
 			e.printStackTrace();
-		} finally {
+		}finally {
 			try {
+				//객체를 사용했던 순서 역순으로 닫아준다.
 				if(rs!=null)rs.close();
 				if(pstmt!=null)pstmt.close();
 				if(conn!=null)conn.close();
-			} catch (Exception e2) {
-				// TODO: handle exception
-			}
-		}
+			}catch(Exception e) {}
+		}		
+		
 		return dto;
 	}
-	// 회원 목록을 리턴해주는 메소드
+	//회원 목록을 리턴해주는 메소드
 	public List<MemberDto> getList(){
+		//회원 목록을 담을 객체 생성
 		List<MemberDto> list=new ArrayList<>();
+		
 		Connection conn=null;
 		PreparedStatement pstmt=null;
 		ResultSet rs=null;
 		try {
+			//DBConnect 객체를 이용해서 Connection 객체의 참조값을 얻어온다.
 			conn=new DBConnect().getConn();
-			String sql="SELECT *"
+			//실행할 sql 문
+			String sql="SELECT num,name,addr"
 					+ " FROM member"
 					+ " ORDER BY num ASC";
 			pstmt=conn.prepareStatement(sql);
-			pstmt.executeQuery();
+			//query 문 수행하고 결과 얻어오기
+			rs=pstmt.executeQuery();
+			//반복문 돌면서 select 된 회원정보  읽어오기
 			while(rs.next()) {
+				//회원정보를 list 에 담아 보세요.
 				int num=rs.getInt("num");
 				String name=rs.getString("name");
 				String addr=rs.getString("addr");
+				//MemberDto 객체를 생성해서 회원 한명의 정보를 담는다.
 				MemberDto dto=new MemberDto();
 				dto.setNum(num);
 				dto.setName(name);
 				dto.setAddr(addr);
+				//MemberDto 객체를 List 에 누적 시킨다.
 				list.add(dto);
 			}
-			System.out.println("member 테이블의 회원 목록을 출력합니다.");
-			for(MemberDto tmp:list) {
-				System.out.println(tmp.getNum()+" | "+tmp.getName()+" | "+tmp.getAddr());
-			}
-		} catch (Exception e) {
+		}catch(Exception e) {
 			e.printStackTrace();
-		} finally {
+		}finally {
 			try {
+				//객체를 사용했던 순서 역순으로 닫아준다.
 				if(rs!=null)rs.close();
 				if(pstmt!=null)pstmt.close();
 				if(conn!=null)conn.close();
-			} catch (Exception e2) {
-			}
+			}catch(Exception e) {}
 		}
-		return printMember(list);
+		
+		return list;
 	}
-	// 회원목록을 콘솔창에 출력해주는 메소드
-	public static void printMember(List<MemberDto> list) {
-		for(MemberDto tmp:list) {
-			System.out.println(tmp.getNum()+" | "+tmp.getName()+" | "+tmp.getAddr());
-		}
-	}
-	// 회원 정보를 DB에서 삭제하는 메소드
+	
+	//회원 한명의 정보를 DB 에서 삭제하는 메소드
 	public void delete(int num) {
 		Connection conn=null;
 		PreparedStatement pstmt=null;
@@ -120,18 +133,17 @@ public class MemberDao {
 			pstmt.setInt(1, num);
 			pstmt.executeUpdate();
 			System.out.println("회원 정보를 삭제 했습니다.");
-		} catch (Exception e) {
+		}catch(Exception e) {
 			e.printStackTrace();
-		} finally {
+		}finally {
 			try {
 				if(pstmt!=null)pstmt.close();
 				if(conn!=null)conn.close();
-			} catch (Exception e2) {
-				// TODO: handle exception
-			}
+			}catch(Exception e) {}
 		}
 	}
-	// 회원 정보를 DB에 저장하는 메소드
+	
+	//회원 정보를 DB 에 저장하는 메소드 
 	public void insert(MemberDto dto) {
 		Connection conn=null;
 		PreparedStatement pstmt=null;
@@ -144,19 +156,17 @@ public class MemberDao {
 			pstmt.setString(1, dto.getName());
 			pstmt.setString(2, dto.getAddr());
 			pstmt.executeUpdate();
-			System.out.println("회원 정보를 저장했습니다");
-		} catch (Exception e) {
+			System.out.println("회원 정보를 추가 했습니다.");
+		}catch(Exception e) {
 			e.printStackTrace();
-		} finally {
+		}finally {
 			try {
 				if(pstmt!=null)pstmt.close();
 				if(conn!=null)conn.close();
-			} catch (Exception e2) {
-				// TODO: handle exception
-			}
+			}catch(Exception e) {}
 		}
 	}
-	// 회원 정보를 DB에서 수정하는 메소드
+	//회원 정보를 DB 에서 수정하는 메소드
 	public void update(MemberDto dto) {
 		Connection conn=null;
 		PreparedStatement pstmt=null;
@@ -170,16 +180,14 @@ public class MemberDao {
 			pstmt.setString(2, dto.getAddr());
 			pstmt.setInt(3, dto.getNum());
 			pstmt.executeUpdate();
-			System.out.println("회원 정보를 업데이트 했습니다.");
-		} catch (Exception e) {
+			System.out.println("회원 정보를 수정했습니다.");
+		}catch(Exception e) {
 			e.printStackTrace();
-		} finally {
+		}finally {
 			try {
 				if(pstmt!=null)pstmt.close();
 				if(conn!=null)conn.close();
-			} catch (Exception e2) {
-				// TODO: handle exception
-			}
-		}
+			}catch(Exception e) {}
+		}		
 	}
 }
